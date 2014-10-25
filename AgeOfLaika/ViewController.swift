a51a21318488824759371ba73cd16f79121a7335
Thinh Le <Tle9@Thinhs-MacBook-Pro.local>
2014-10-25 14:25:33 -0500

    Testing git

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var humanYears: UITextField!
+    @IBOutlet weak var convertedDogYears: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertToDogYears(sender: UIButton) {
+        
+        let yearFromTextField = humanYears.text.toInt()!
+        let conversionConstant = 7
+        convertedDogYears.hidden = false
+        
+        convertedDogYears.text = "\(yearFromTextField + conversionConstant)" + " in Dog Years"
+        
+        // testing git push
+        
+    }
 
 }
 
